2ec6b04b765562e5e1a567b9b4e3791e756d9f78
root <jakobgebler@mail.de>
2016-11-22 18:06:15 +0100

    Vecor (name: allQuestions) vom Typ Question mit allen Daten aus der Datenbank

--- app/src/main/java/com/example/root/testgit/MainActivity.java
+++ app/src/main/java/com/example/root/testgit/MainActivity.java
@@ -1,11 +1,9 @@
 package com.example.root.testgit;
 
 import android.content.Intent;
-import android.support.design.widget.FloatingActionButton;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity{
 
