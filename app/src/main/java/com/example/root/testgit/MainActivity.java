a4c84c0d93176144cf0633a65259cdd453c99d51
root <jakobgebler@mail.de>
2016-11-23 21:04:03 +0100

    Main Activity geordnet

--- app/src/main/java/com/example/root/testgit/MainActivity.java
+++ app/src/main/java/com/example/root/testgit/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -12,12 +13,13 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
 import java.util.Vector;
 import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity{
 
-    Vector<Question> allQuestions = new Vector();
+    ArrayList<Question> allQuestions = new ArrayList();
     public interface AsyncResponse {
         void processFinish(String output) throws JSONException;
     }
@@ -27,28 +29,30 @@ public class MainActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        FillQuestionArrayList();
+        FillListView(allQuestions);
+
+         //handle listview and assign adapter
+        //ListView lView = (ListView)findViewById(R.id.listdata);
+        //lView.setAdapter((ListAdapter) allQuestions);
+    }
+
+    public void vSwitchToEdit(View view) {
+
+        Intent showCreate = new Intent(this, EditActivity.class);
+        startActivity(showCreate);
+    }
+
+    // --- with vGetData - LOAD ALL DATA from SERVER and SAVE in 'allWuestions' ---
+    public void FillQuestionArrayList(){
+        // --- LOAD JSON STRING FROM SERVER and CONVERT to QUESTION VECTOR ---
+        // --- STRING SAVED on 'gesammt' --- QUESTION VEC on 'allQuestion' ---
+
         String whatver = "";
-
+        JSONObject QuesData  = null;
         try {
-            whatver = vGetData(this.findViewById(R.id.activity_main).getRootView());
-        } catch (ExecutionException e) {
-            e.printStackTrace();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        TextView test = (TextView) findViewById(R.id.textView2);
-
-        JSONObject jObject  = null;
-        try {
-            jObject = new JSONObject(whatver);
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-        String gesammt = "";
-
-        try {
-            JSONArray result = jObject.getJSONArray("server_response");
+            QuesData = new JSONObject( whatver = vGetData(this.findViewById(R.id.activity_main).getRootView()));
+            JSONArray result = QuesData.getJSONArray("server_response");
             String ques,ans1,ans2;
 
             for(int i=0;i<result.length();i++)
@@ -61,39 +65,15 @@ public class MainActivity extends AppCompatActivity{
 
                 // --- test ausgabe eines elementes aus allQuestions ---
                 //((Question) (allQuestions.elementAt(i))).displayQuestion();
-                gesammt += " -> " + ques + " - " + ans1 + " - " + ans2 + " -------" + allQuestions.size() + "\n";
             }
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         } catch (JSONException e) {
             e.printStackTrace();
         }
-        test.setText(gesammt);
-
-        System.out.println("" + whatver);
-        System.out.println(" --- " + allQuestions.size());
-
-        String[] QuesList = new String[allQuestions.size()];
-        String[] Ans1List = new String[allQuestions.size()];
-        String[] Ans2List = new String[allQuestions.size()];
-
-        for(int i = 0;i<allQuestions.size();i++){
-            QuesList[i] = ((Question) allQuestions.elementAt(i)).getQuestion();
-            Ans1List[i] = ((Question) allQuestions.elementAt(i)).getAnswer1();
-            Ans2List[i] = ((Question) allQuestions.elementAt(i)).getAnswer2();
-        }
-
-        ListView listView1 = (ListView) findViewById(R.id.listdata);
-
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, QuesList);
-
-        listView1.setAdapter(adapter);
     }
-
-    public void vSwitchToEdit(View view) {
-
-        Intent showCreate = new Intent(this, EditActivity.class);
-        startActivity(showCreate);
-    }
-
 
     public String vGetData(View view) throws ExecutionException, InterruptedException {
         ReadData data = new ReadData(new AsyncResponse() {
@@ -103,5 +83,24 @@ public class MainActivity extends AppCompatActivity{
             }
         });
         return data.execute().get();
+    }
+
+    public void FillListView(ArrayList<Question> quesArrayList){
+        // --- QUESTION LIST ARRAY to 3 STRINGS ---
+        String[] QuesList = new String[allQuestions.size()];
+        String[] Ans1List = new String[allQuestions.size()];
+        String[] Ans2List = new String[allQuestions.size()];
+
+        for(int i = 0;i<allQuestions.size();i++){
+            QuesList[i] = ((Question) allQuestions.get(i)).getQuestion();
+            Ans1List[i] = ((Question) allQuestions.get(i)).getAnswer1();
+            Ans2List[i] = ((Question) allQuestions.get(i)).getAnswer2();
+        }
+
+        ListView listView1 = (ListView) findViewById(R.id.listdata);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, QuesList);
+
+        listView1.setAdapter(adapter);
     }
 }
